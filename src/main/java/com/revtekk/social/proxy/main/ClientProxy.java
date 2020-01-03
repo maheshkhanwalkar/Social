ec58ee5a7afc75a6c9face2422a292b038277660
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2020-01-03 16:31:04 -0500

    proxy: remove NioFlex-related code

--- src/main/java/com/revtekk/social/proxy/main/ClientProxy.java
+++ src/main/java/com/revtekk/social/proxy/main/ClientProxy.java
@@ -1,9 +1,5 @@
 package com.revtekk.social.proxy.main;
 
-import com.revtekk.nioflex.config.SecurityType;
-import com.revtekk.nioflex.config.SocketType;
-import com.revtekk.nioflex.impl.ServerBuilder;
-import com.revtekk.nioflex.main.Server;
 import com.revtekk.social.config.ServerConf;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,13 +8,10 @@ import org.yaml.snakeyaml.constructor.Constructor;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.InetAddress;
 
 public class ClientProxy
 {
     private static final Logger LOG = LoggerFactory.getLogger(ClientProxy.class);
-
-    private static Server proxy;
     private static ServerConf conf;
 
     public static void main(String[] args) throws IOException
@@ -37,21 +30,9 @@ public class ClientProxy
         conf = yml.load(new FileInputStream("src/main/resources/config/proxy.yml"));
     }
 
-    private static void startServer() throws IOException
+    private static void startServer()
     {
         LOG.info("Initialising server");
-
-        proxy = ServerBuilder.build(
-                InetAddress.getByName(conf.getIP()), conf.getPort(), SocketType.SOCKET_TCP,
-                SecurityType.SECURITY_NONE, new Reader());
-
-        if(proxy == null)
-        {
-            LOG.error("Could not create server: check configuration and/or port already bound");
-            System.exit(-1);
-        }
-
-        LOG.info("Starting server");
-        proxy.start();
+        // TODO start a server and add hooks
     }
 }
--- src/test/java/com/revtekk/social/Client.java
+++ src/test/java/com/revtekk/social/Client.java
@@ -1,48 +1,15 @@
 package com.revtekk.social;
 
-import com.google.gson.Gson;
 import com.revtekk.social.proxy.router.Format;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.Socket;
-import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Client
 {
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args)
     {
-        Socket proxy = new Socket("localhost", 10002);
-
-        OutputStream os = proxy.getOutputStream();
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os));
-
-        Format fmt = makeObject();
-        Gson gson = new Gson();
-
-        String raw = gson.toJson(fmt);
-        System.out.println(raw);
-
-        writeString(os, bw, raw);
-        bw.close();
-    }
-
-    private static void writeString(OutputStream os, BufferedWriter bw, String str)
-            throws IOException
-    {
-        int len = str.length();
-
-        ByteBuffer buf = ByteBuffer.allocate(4);
-        buf.putInt(len);
-
-        os.write(buf.array(), 0, 4);
-        bw.write(str);
-
-        bw.flush();
+        // TODO implement a simple client
     }
 
     private static Format makeObject()
